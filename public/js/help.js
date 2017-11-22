ca9eaf4039e56bb38f45c81e34f6fbfa1e1a8eef
Jose <murrietj@oregonstate.edu>
2017-11-21 19:15:17 -0800

    fixed help guided walkthrough

--- public/js/help.js
+++ public/js/help.js
@@ -1,5 +1,9 @@
 var step = 0;
 var explainText = document.getElementById('explain');
+var btn;
+var btnTextColorDefault;
+const btnBgColorActive = "#28a745";
+const btnTextColorActive = "white";
 
 function noGuided(){
   document.getElementById('btns').remove();
@@ -15,40 +19,54 @@ function startGuided(){
       break;
     }
     case 1:{
-      document.getElementById('home').style.backgroundColor = "#5856d6";
+      setNewActiveButton(btn, 'home');
       explainText.textContent = "This is the Home page. It is your dashboard for progress/notifications and access to the discussion board.";
       break;
     }
     case 2:{
-      document.getElementById('home').style.backgroundColor = null;
-      document.getElementById('courses').style.backgroundColor = "#5856d6";
-      explainText.textContent = "This is the Courses page. This is where your lectures, projects, and interview questions are located.";
+      setNewActiveButton(btn, 'courses');
+      explainText.textContent = "This is the Courses page. This is where you can view all the courses that you are currently enrolled in.";
       break;
     }
     case 3:{
-      document.getElementById('courses').style.backgroundColor = null;
-      document.getElementById('profile').style.backgroundColor = "#5856d6";
-      explainText.textContent = "This is the Profile page. This is where you change your personal account settings.";
+      setNewActiveButton(btn, 'projects');
+      explainText.textContent = "This is the Projects page. This is where you can view all your active projects for your active course.";
       break;
     }
     case 4:{
-      document.getElementById('profile').style.backgroundColor = null;
-      document.getElementById('help').style.backgroundColor = "#5856d6";
-      explainText.textContent = "This is the Help page. This is where you guided tour of all the website features.";
+      setNewActiveButton(btn, 'profile');
+      explainText.textContent = "This is the Profile page. This is where you change your personal account settings.";
       break;
     }
     case 5:{
-      document.getElementById('help').style.backgroundColor = null;
-      document.getElementById('logout').style.backgroundColor = "#5856d6";
-      explainText.textContent = "This is the Logout button. This is where you logout out of your session.";
+      setNewActiveButton(btn, 'help');
+      explainText.textContent = "This is the Help page. This is where you can go through a guided tour of all the website features.";
       break;
     }
     case 6:{
-      document.getElementById('logout').style.backgroundColor = null;
+      setNewActiveButton(btn, 'logout');
+      explainText.textContent = "This is the Logout button. This is where you logout out of your session.";
+      break;
+    }
+    case 7:{
+      btn.style.backgroundColor = null;
+      btn.style.color = btnTextColorDefault;
       document.getElementById('nextBtn').remove();
       explainText.textContent = "This is the end of the guided tour. Stop by whenever you need help.";
       break;
     }
   }
   step++;
+}
+
+function setNewActiveButton(oldBtn, newBtnId){
+  if(oldBtn){
+    btn.style.backgroundColor = null;
+    btn.style.color = btnTextColorDefault;
+  }
+
+  btn = document.getElementById(newBtnId);
+  btnTextColorDefault = btn.style.color;
+  btn.style.color = btnTextColorActive;
+  btn.style.backgroundColor = btnBgColorActive;
 }
