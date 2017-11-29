65b11e431fd296dffec49815c7eac047c7232206
mheinemann28 <heinemam@oregonstate.edu>
2017-11-28 22:40:48 -0500

    updated sql

--- db.sql
+++ db.sql
@@ -1,113 +1,172 @@
--- MySQL dump 10.13  Distrib 5.7.20, for osx10.13 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 2.11.9.4
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: cs361
--- ------------------------------------------------------
--- Server version	5.7.20
+-- Host: oniddb
+-- Generation Time: Nov 28, 2017 at 07:37 PM
+-- Server version: 5.5.55
+-- PHP Version: 5.2.6-1+lenny16
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `heinemam-db`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Courses`
 --
 
-DROP TABLE IF EXISTS `Courses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Courses` (
-  `ID` int(11) NOT NULL,
-  `CourseName` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `projects`
---
-
-DROP TABLE IF EXISTS `projects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `projects` (
-  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `title` tinytext NOT NULL,
-  `instructions` text NOT NULL,
+CREATE TABLE IF NOT EXISTS `Courses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Courses`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Lectures`
+--
+
+CREATE TABLE IF NOT EXISTS `Lectures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `youtubeURL` varchar(255) NOT NULL,
+  `courseID` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `courseID` (`courseID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `Lectures`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Projects`
+--
+
+CREATE TABLE IF NOT EXISTS `Projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `courseID` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `courseID` (`courseID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `Projects`
+--
+
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `sessions`
 --
 
-DROP TABLE IF EXISTS `sessions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sessions` (
+CREATE TABLE IF NOT EXISTS `sessions` (
   `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `expires` int(11) unsigned NOT NULL,
   `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `user_project`
+-- Dumping data for table `sessions`
 --
 
-DROP TABLE IF EXISTS `user_project`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_project` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` tinyint(3) unsigned NOT NULL,
-  `project_id` tinyint(3) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `User`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `User` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` binary(60) NOT NULL,
-  `course_id` int(11) DEFAULT NULL,
+  `activeCourse` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Dumping data for table `User`
+--
 
--- Dump completed on 2017-11-24  2:38:14
 
-DROP TABLE IF EXISTS `Lessons`;
+-- --------------------------------------------------------
 
-CREATE TABLE `Lessons` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `path` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+--
+-- Table structure for table `Users_Course`
+--
+
+CREATE TABLE IF NOT EXISTS `Users_Course` (
+  `courseID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `isEnrolled` tinyint(1) NOT NULL,
+  KEY `courseID` (`courseID`,`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Users_Course`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users_Lectures`
+--
+
+CREATE TABLE IF NOT EXISTS `Users_Lectures` (
+  `userID` int(11) DEFAULT NULL,
+  `lectureID` int(11) DEFAULT NULL,
+  `isWatched` tinyint(1) NOT NULL,
+  KEY `UserID` (`userID`),
+  KEY `LectureID` (`lectureID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Users_Lectures`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users_Project`
+--
+
+CREATE TABLE IF NOT EXISTS `Users_Project` (
+  `userID` int(3) unsigned NOT NULL,
+  `projectID` int(11) unsigned NOT NULL,
+  `isSubmitted` tinyint(1) NOT NULL,
+  `grade` float NOT NULL,
+  KEY `userID` (`userID`),
+  KEY `projectID` (`projectID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Users_Project`
+--
 
